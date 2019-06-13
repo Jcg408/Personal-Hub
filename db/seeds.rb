eacd72d394e962e1dfc954dc8420a9c272c349c6
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-06-13 19:49:19 -0400

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+quotes = Quote.create([
+    {saying: "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.", author: "Jimmy Dean"}, 
+    {saying: "I will love the light for it shows me the way, yet I will endure the darkness because it shows me the stars.", author: "Og Mandino"}, 
+    {saying: "All you need is the plan, the road map, and the courage to press on to your destination.", author: "Earl Nightingale"}
+])
+    
+
+
+contacts = Contact.create([
+    {firstname: "Judy", lastname: "Smith", phone: "540-111-2222", email: "jsmith@wow.com"},
+    {firstname: "John", lastname: "Doe", phone: "540-333-4444", email: "jdoe@wow.com"},
+    {firstname: "Lily", lastname: "Rose", phone: "540-555-6666", email: "lily@wow.com"}
+])
+
+photos = Photo.create([
+    {title: "mountains and lake", url: "https://temp.media/?height=400&width=500&text=&category=nature&color"},
+    {title: "tabby cat", url: "https://temp.media/?height=400&width=500&text=&category=animals&color"},
+    {title: "dinner with friends", url: "https://temp.media/?height=400&width=500&text=&category=food&color"}
+])
